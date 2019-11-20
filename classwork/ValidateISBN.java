7991e864dd9ebc562f577d46084d7702b79408b1
ray-abel12 <rayabel12@outlook.com>
2019-11-20 19:42:52 +0100

    update

--- classwork/ValidateISBN.java
+++ classwork/ValidateISBN.java
@@ -6,6 +6,7 @@ public class ValidateISBN {
 		if (isbn.length() !=10)throw new NumberFormatException("Isbn number must be 10 digit long");
 		int total = 0;
 		for(int i = 0;i < 10;i++) {
+			if(!Character.isDigit(isbn.charAt(i))) throw new NumberFormatException("must be digit");
 		total += isbn.charAt(i) * (10 -i);
 		}
 		if(total % 11 ==0 ) {
