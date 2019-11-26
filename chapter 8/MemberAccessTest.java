590aee6e1622753e5ecf4c8be5e2496abed36031
ray-abel12 <rayabel12@outlook.com>
2019-11-26 05:11:06 +0100

    update

---
+++ chapter 8/MemberAccessTest.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class MemberAccessTest {
+
+	public static void main(String[] args) {
+		Time1 time = new Time1();
+		time.hour = 10;
+		time.minute = 13;
+		time.second = 17;
+
+	}
+}
