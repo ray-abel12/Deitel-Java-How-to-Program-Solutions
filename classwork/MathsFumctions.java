125bd5bad88ce4eb64b353e35cb67f651d74eb0e
ray-abel12 <rayabel12@outlook.com>
2019-11-30 22:04:01 +0100

    initial commit

---
+++ classwork/MathsFumctions.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class MathsFumctions {
+
+	public static void main(String[] args) {
+		System.out.println(0.00/0.00);
+		System.out.println(Math.sqrt(100.00));
+		System.out.println(Math.floor(100.4));
+		System.out.println(Math.min(100, 200));
+		System.out.println(Math.max(200, 466));
+	}
+
+}
