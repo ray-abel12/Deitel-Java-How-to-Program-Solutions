617b41b90a5a22945ee711313c490d028f1a2ff8
ray-abel12 <rayabel12@outlook.com>
2019-12-04 10:32:58 +0100

    commit

---
+++ spring/BaseballCoach.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class BaseballCoach {
+	
+	public String getDailyWorkout() {
+		
+		return "Spend 30 minutes on batting pratice";
+	}
+}
---
+++ spring/MyApp.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// create the object 
+		
+		BaseballCoach thecoach = new BaseballCoach();
+		 
+		//use the object created
+		System.out.println(thecoach.getDailyWorkout());
+
+	}
+
+}
