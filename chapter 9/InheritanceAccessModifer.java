e22d00a6231c79293e6cbfdc76eab42282f68f75
ray-abel12 <rayabel12@outlook.com>
2019-12-11 16:18:24 +0100

    initial commit

---
+++ chapter 9/InheritanceAccessModifer.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class InheritanceAccessModifer {
+	static private int privateMember = 2;
+	static  int defaultMember = 12;
+	static protected int protectedMember = 26;
+	public static int publicMember = 12;
+}
---
+++ chapter 9/InheritanceAccessModiferTest.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class InheritanceAccessModiferTest extends InheritanceAccessModifer {
+
+	public static void main(String[] args) {
+		
+		System.out.println(InheritanceAccessModiferTest.protectedMember);
+		System.out.println(InheritanceAccessModiferTest.defaultMember);
+		System.out.println(InheritanceAccessModiferTest.publicMember);
+	//	System.out.println(InheritanceAccessModiferTest.privateMember);
+
+	}
+
+}
