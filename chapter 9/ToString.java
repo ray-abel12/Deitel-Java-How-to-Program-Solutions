533406e9caec770977addcc211215b6424031664
ray-abel12 <rayabel12@outlook.com>
2019-12-12 07:36:33 +0100

    update

--- chapter 9/ToString.java
+++ chapter 9/ToString.java
@@ -1,8 +1,13 @@
 package exercises;
 
 public class ToString {
+	 
+	 public static void main(String[] args) {
+		 ToString str = new ToString();
+		 System.out.println(str.toString());
+	 }
+	 
 	 public String toString() {
 		 return String.format("%s","abel");
 	 }
-	
 }
