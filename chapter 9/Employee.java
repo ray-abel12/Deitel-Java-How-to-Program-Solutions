80e72c1524ff8fcee62962e088346565e2f92182
ray-abel12 <rayabel12@outlook.com>
2019-12-13 00:23:26 +0100

    initial commit

---
+++ chapter 9/Employee.java
@@ -0,0 +1,18 @@
+package exercises;
+
+public class Employee {
+	private String name;
+	private int salary;
+	
+Employee(String n,int s){
+	this.name = n;
+	this.salary = s;
+}
+
+	public String getName() {
+	return name;
+}
+	public int getSalary() {
+		return salary;
+	}
+}
