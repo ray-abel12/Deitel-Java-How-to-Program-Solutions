c026e759850af95f394b5914d3c35734ac6f878d
ray-abel12 <rayabel12@outlook.com>
2019-12-17 13:53:28 +0100

    commit

---
+++ chapter 9/CircleTest.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class CircleTest {
+
+	public static void main(String[] args) {
+		Circle cir = new Circle(12,14,500);
+		System.out.println(cir.getX());
+		System.out.println(cir.getY());
+		System.out.println(cir.getRadius());
+
+	}
+
+}
