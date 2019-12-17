a30e3a95559ea43588e166b7eba96ce9945fd994
ray-abel12 <rayabel12@outlook.com>
2019-12-17 13:43:55 +0100

    commit

---
+++ chapter 9/RectangleTest.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class RectangleTest {
+
+	public static void main(String[] args) {
+	Rectangle r1 = new Rectangle(200,100,14,17);
+    System.out.println(r1.getX());
+    System.out.println(r1.getY());
+    System.out.println(r1.getHeight());
+    System.out.println(r1.getWidth());
+	}
+
+}
