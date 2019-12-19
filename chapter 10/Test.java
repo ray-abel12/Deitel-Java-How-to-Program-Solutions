1f04207c18e1b4e20fa725bce98a8c485f7d6546
ray-abel12 <rayabel12@outlook.com>
2019-12-19 19:52:39 +0100

    update

--- chapter 10/Test.java
+++ chapter 10/Test.java
@@ -3,10 +3,10 @@ package exercise;
 public class Test {
 
 	public static void main(String[] args) {
-	Shape s1 = new Shape(2,5);
+	//Shape s1 = new Shape(2,5);
 	Circle c1 = new Circle(10,20,5);
 	Rectangle r1 = new Rectangle(10,20,24,25);
-    s1.draw();
+  //  s1.draw();
     System.out.println();
     c1.draw();
     System.out.println();
