e546604cf5c6522f957031a9d5474a3ac3dc79b5
ray-abel12 <rayabel12@outlook.com>
2019-12-19 18:47:06 +0100

    update

---
+++ chapter 10/Test.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class Test {
+
+	public static void main(String[] args) {
+	Shape s1 = new Shape(2,5);
+	Circle c1 = new Circle(10,20,5);
+	Rectangle r1 = new Rectangle(10,20,24,25);
+    s1.draw();
+    c1.draw();
+    r1.draw();
+    
+	}
+
+}
