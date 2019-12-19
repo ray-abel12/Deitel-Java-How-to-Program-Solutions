0a3cee70fe739b3fb81fbce88ffe7046501f4ab0
ray-abel12 <rayabel12@outlook.com>
2019-12-19 21:29:11 +0100

    commit

---
+++ chapter 10/RectangleInterface.java
@@ -0,0 +1,35 @@
+package exercise;
+
+public class RectangleInterface implements ShapeInterface {
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setX(int xcor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setY(int ycor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
