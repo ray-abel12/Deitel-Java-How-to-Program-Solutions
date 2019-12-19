f113da0843a1bfbbb208583075ae8b2df85fe351
ray-abel12 <rayabel12@outlook.com>
2019-12-19 19:46:58 +0100

    update

--- chapter 10/AbstractShapeTest.java
+++ chapter 10/AbstractShapeTest.java
@@ -1,6 +1,10 @@
 package exercise;
 
 public class AbstractShapeTest {
-	//can not instantiate it because we used the keyword abstract
-Shape shapes = new Shape(12,7);
+	public static void main(String... args) {
+	//can not instantiate it because we used the keyword abstract but you can use it to create other classes
+   //Shape shapes = new Shape(12,7);
+	Circle c1 = new Circle(12,13,14);
+	c1.draw();
+}
 }
