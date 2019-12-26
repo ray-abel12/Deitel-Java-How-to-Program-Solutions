f0b4e8e39d9ec95855a22149590928a91373bfb7
ray-abel12 <rayabel12@outlook.com>
2019-12-26 15:42:10 +0100

    initial commit

--- chapter 10/abstact/AbstactSubclass.java
+++ chapter 10/abstact/AbstactSubclass.java
@@ -1,5 +1,17 @@
 package abstact;
 
-public class AbstactSubclass {
+public class AbstactSubclass extends AbstactSuperclass {
+
+	@Override
+	void test1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void test2() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
