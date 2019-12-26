d43a3cfe0e92a2082ce33dc9ac72f61da3ebb366
ray-abel12 <rayabel12@outlook.com>
2019-12-26 06:57:12 +0100

    initial commit

---
+++ chapter 10/exercise1/User.java
@@ -0,0 +1,7 @@
+package exercise1;
+
+public class User {
+	User(){
+		
+	}
+}
---
+++ chapter 10/exercise1/UserTest.java
@@ -0,0 +1,11 @@
+package exercise1;
+
+public class UserTest {
+
+	public static void main(String[] args) {
+		User play = new User();
+		User hard = new Love();
+
+	}
+
+}
