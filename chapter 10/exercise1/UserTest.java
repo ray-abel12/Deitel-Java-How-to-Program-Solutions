eb39d026c2c6a75d26751078b514b01f002ed4e5
ray-abel12 <rayabel12@outlook.com>
2019-12-26 07:27:15 +0100

    initial commit

--- chapter 10/exercise1/UserTest.java
+++ chapter 10/exercise1/UserTest.java
@@ -1,9 +1,12 @@
 package exercise1;
 
 public class UserTest {
+	public void printUserType(User u) {
+		u.printUserType();
+	}
 
 	public static void main(String[] args) {
-		User user = new Staff();
+		//User user = new Staff();
 
 	}
 
