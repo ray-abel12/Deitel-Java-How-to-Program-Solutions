d33ae3d385943d41181e70612adb471a1d4d1c52
ray-abel12 <rayabel12@outlook.com>
2019-12-26 07:23:36 +0100

    initial commit

--- chapter 10/exercise1/Editor.java
+++ chapter 10/exercise1/Editor.java
@@ -1,5 +1,11 @@
 package exercise1;
 
-public class Editor extends User {
-
+public class Editor extends Staff {
+	public void printUserType() {
+		System.out.println("Editor");
 }
+	public void approveReviews() {
+		
+	}
+}
+
